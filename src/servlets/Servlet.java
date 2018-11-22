c53912ef597389446b321cc8f8475d6689e3aaf1
nelsin12 <45272996+nelsin12@users.noreply.github.com>
2018-11-22 18:48:06 -0400

    fixservletlogin

--- src/servlets/Servlet.java
+++ src/servlets/Servlet.java
@@ -6,24 +6,47 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.jboss.resteasy.spi.HttpResponse;
 
 import ejb.*;
 import dao.*;
 
 import javax.servlet.RequestDispatcher;
 
-public class Servlet extends HttpServlet{
+public class Servlet extends HttpServlet{ 
 	
-	public boolean login(String rut, String pass){
+	
+	
+	
+	public void doPost(HttpServletRequest request,HttpResponse response){
+		
+		String rut = request.getParameter("rut");
+		String pass = request.getParameter("pass");
 		EjbCliente cliente = new EjbCliente();
 		cliente.setRut_persona(rut);
 		cliente.setClave_cliente(pass);
 		DaoCliente daocliente = new DaoCliente();
-		return daocliente.Logins(cliente);
+		if(daocliente.Logins(cliente)){
+			RequestDispatcher request_dispatcher = request.getRequestDispatcher("index.html");
+			try {
+				request_dispatcher.forward(request, (ServletResponse) response);
+			} catch (ServletException | IOException e) {				
+				e.printStackTrace();
+			}
+		}else{
+			RequestDispatcher request_dispatcher = request.getRequestDispatcher("login.html");
+			try {
+				request_dispatcher.forward(request, (ServletResponse) response);
+			} catch (ServletException | IOException e) {				
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
